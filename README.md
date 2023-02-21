fc7ee5606de00c0fdad881d52b4376674ffd76dc
Jay Manalansan <jaypatrickm@gmail.com>
2023-02-21 17:29:31 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+<!-- PROJECT LOGO -->
+<p align="center">
+  <h3 align="center">Streak Counter</h3>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Table of Contents](#table-of-contents)
+  - [Built With](#built-with)
+- [Getting Started](#getting-started)
+  - [Installation](#installation)
+  - [Usage](#usage)
+- [Contact](#contact)
+
+### Built With
+
+-  [TypeScript](https://typescriptlang.org/)
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+### Installation
+
+1. Clone the repo
+
+```sh
+git clone [add repo link]
+```
+
+3. Install NPM packages
+
+```sh
+npm install
+```
+
+4. To open the project in development
+
+```sh
+npm run 
+```
+
+### Usage
+
+1. Add Usage Examples here
+
+<!-- CONTACT -->
+
+## Contact
+
+Jay Manalansan - jaypatrickm@gmail.com
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { formattedDate, shouldIncrementOrResetStreakCount, STREAK_KEY, buildStreak } from "./lib";
+
+export interface Streak {
+  currentCount: number
+  startDate: string
+  lastLoginDate: string
+}
+
+export function streakCounter(_localStorage: Storage, date: Date): Streak {
+  const streakInLocalStorage = _localStorage.getItem(STREAK_KEY);
+  if(streakInLocalStorage) {
+    try {
+      const streak = JSON.parse(streakInLocalStorage || "") as Streak;
+      const STATE = shouldIncrementOrResetStreakCount(date, streak.lastLoginDate)
+      
+      let updatedStreak = <Streak>{}
+      
+      switch (STATE) {
+        case 'increment': {
+          updatedStreak = buildStreak(date, {
+                currentCount: streak.currentCount += 1
+          })
+          _localStorage.setItem(STREAK_KEY, JSON.stringify(updatedStreak))
+          break;
+        }
+        case 'reset': {
+          updatedStreak = buildStreak(date)
+          _localStorage.setItem(STREAK_KEY, JSON.stringify(updatedStreak))
+          break;
+        }
+        case 'none': 
+        default: {
+          updatedStreak = streak
+        }
+      }
+
+      return updatedStreak
+    } catch (error) {
+      console.error("Failed to parse streak from localStorage")
+    }
+  }
+  
+  const streak = {
+    currentCount: 1,
+    startDate: formattedDate(date),
+    lastLoginDate: formattedDate(date)
+  }
+
+  _localStorage.setItem(STREAK_KEY, JSON.stringify(streak))
+  return streak
+}
